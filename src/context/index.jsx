43f75715857356a016baccac243bda1710b38ca5
Sachin Bankoti <sachinbankoti16@gmail.com>
2024-03-25 10:03:45 -0400

    setting up the details page

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -3,31 +3,43 @@ import { useState } from "react";
 
 export const GlobalContext = createContext(null);
 
+export default function GlobalState({ children }) {
+  const [searchParam, setSearchParam] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
+  const [recipeDetailsData, setRecipeDetailsData] = useState(null);
 
-export default function GlobalState({children}){
-    const[searchParam, setSearchParam] = useState("");
-    const [ loading, setLoading] = useState(false);
-    const [ recipeList, setRecipeList] = useState([]);
-
-    const handleSubmit = async(e)=>{
-        e.preventDefault();
-        try {
-            const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`);
-            const data = await res.json();
-            console.log(data);
-            if (data?.data?.recipes) {
-                setRecipeList(data?.data?.recipes);
-                setLoading(false);
-                setSearchParam('');
-            }
-        } catch (error) {
-            console.log(error);
-        }
-        console.log(loading,recipeList);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
+      );
+      const data = await res.json();
+      console.log(data);
+      if (data?.data?.recipes) {
+        setRecipeList(data?.data?.recipes);
+        setLoading(false);
+        setSearchParam("");
+      }
+    } catch (error) {
+      console.log(error);
     }
-    return(
-        <GlobalContext.Provider value={{searchParam,setSearchParam,handleSubmit,loading,recipeList}}>
-            {children}
-        </GlobalContext.Provider>
-    )
+    console.log(loading, recipeList);
+  };
+  return (
+    <GlobalContext.Provider
+      value={{
+        searchParam,
+        setSearchParam,
+        handleSubmit,
+        loading,
+        recipeList,
+        recipeDetailsData,
+        setRecipeDetailsData,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 }
